25347d36fbfcfa13389c6156738954d190439391
JSooCha <chajinsoo14@gmail.com>
2023-06-06 14:55:26 +0900

    .

--- server/src/main/java/com/codea/member/MemberController.java
+++ server/src/main/java/com/codea/member/MemberController.java
@@ -51,7 +51,7 @@ public class MemberController {
         AddressDto.Post addressDto = new AddressDto.Post(requestBody.getStreetAddress(), requestBody.getLatitude(), requestBody.getLongitude());
         Address address = addressMapper.addressPostDtoToAddress(addressDto);
 
-        String imageUrl = "https://main022.s3.ap-northeast-2.amazonaws.com/image/default_profile.png";
+        String imageUrl = "http://main22.s3-website.ap-northeast-2.amazonaws.com/image/default_profile.png";
         if (requestBody.getBase64Image() != null && requestBody.getImageName() != null && !(requestBody.getBase64Image().isEmpty()) && !(requestBody.getImageName().isEmpty())) {
             imageUrl = imageService.uploadImage(requestBody.getImageName(), requestBody.getBase64Image(), requestBody.getEmail());
         }
