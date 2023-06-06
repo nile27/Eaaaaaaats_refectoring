7f130e3beaa1b8909c633bd5810c19846abc437a
JSooCha <chajinsoo14@gmail.com>
2023-06-06 14:18:38 +0900

    server 추가

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -5,7 +5,7 @@ INSERT INTO CATEGORY (category_id, name) VALUES(4, '중식');
 INSERT INTO CATEGORY (category_id, name) VALUES(5, '베트남 요리');
 INSERT INTO CATEGORY (category_id, name) VALUES(6, '인도 요리');
 INSERT INTO CATEGORY (category_id, name) VALUES(7, '분식');
-INSERT INTO CATEGORY (category_id, name) VALUES(8, '후식');
+INSERT INTO CATEGORY (category_id, name) VALUES(8, '디저트');
 INSERT INTO CATEGORY (category_id, name) VALUES(9, '퓨전 요리');
 INSERT INTO CATEGORY (category_id, name) VALUES(10, '채식');
 INSERT INTO CATEGORY (category_id, name) VALUES(11, '해물');
